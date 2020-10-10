77aaa1e36cbd6132aa6fcbca3cf5bfe33e954ebd
X-czh <m1a2@outlook.com>
2020-10-10 00:01:38 -0400

    support fields

--- src/main/java/taintAnalysis/Taint.java
+++ src/main/java/taintAnalysis/Taint.java
@@ -1,58 +1,87 @@
 package taintAnalysis;
 
-import soot.Unit;
+import soot.SootField;
+import soot.SootMethod;
 import soot.Value;
+import soot.jimple.InstanceFieldRef;
+import soot.jimple.Stmt;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class Taint {
 
-    private Unit unit;
-    private Value value;
-    private Set<Taint> successors;
+    private final Value value;
+    private final Value base;
+    private final SootField field;
+    private final Stmt stmt;
+    private final SootMethod method;
+    private final Set<Taint> successors;
 
-    public Taint(Unit unit, Value value) {
-        this(unit, value, new HashSet<>());
+    public Taint(Value value, Stmt stmt) {
+        this(value, stmt, null, new HashSet<>());
     }
 
-    public Taint(Unit unit, Value value, Set<Taint> successors) {
-        this.unit = unit;
+    public Taint(Value value, Stmt stmt, SootMethod method) {
+        this(value, stmt, method, new HashSet<>());
+    }
+
+    public Taint(Value value, Stmt stmt, SootMethod method, Set<Taint> successors) {
         this.value = value;
+        this.stmt = stmt;
+        this.method = method;
         this.successors = successors;
-    }
 
-    public Unit getUnit() {
-        return unit;
-    }
-
-    public void setUnit(Unit unit) {
-        this.unit = unit;
+        if (value instanceof InstanceFieldRef) {
+            InstanceFieldRef fieldRef = (InstanceFieldRef) value;
+            this.base = fieldRef.getBase();
+            this.field = fieldRef.getField();
+        } else {
+            this.base = null;
+            this.field = null;
+        }
     }
 
     public Value getValue() {
         return value;
     }
 
-    public void setValue(Value value) {
-        this.value = value;
+    public Stmt getStmt() {
+        return stmt;
+    }
+
+    public Value getBase() {
+        return base;
+    }
+
+    public SootField getField() {
+        return field;
+    }
+
+    public SootMethod getMethod() {
+        return method;
     }
 
     public Set<Taint> getSuccessors() {
         return successors;
     }
 
-    public void setSuccessors(Set<Taint> successors) {
-        this.successors = successors;
-    }
-
     public void addSuccessor(Taint successor) {
         this.successors.add(successor);
     }
 
+    public boolean taints(Value v) {
+        if (v instanceof InstanceFieldRef) {
+            if (base == null || field == null) return false;
+            InstanceFieldRef fieldRef = (InstanceFieldRef) v;
+            return base.equivTo(fieldRef.getBase()) && field.equals(fieldRef.getField());
+        }
+        return value.equivTo(v);
+    }
+
     @Override
     public String toString() {
-        return unit.toString() + ":" + value.toString();
+        return value + " in " + stmt + " in method " + method;
     }
 
 }
--- src/main/java/taintAnalysis/TaintFlowAnalysis.java
+++ src/main/java/taintAnalysis/TaintFlowAnalysis.java
@@ -1,26 +1,22 @@
 package taintAnalysis;
 
 import configInterface.ConfigInterface;
-import soot.Body;
-import soot.Unit;
-import soot.Value;
-import soot.ValueBox;
+import soot.*;
 import soot.jimple.*;
 import soot.toolkits.graph.ExceptionalUnitGraph;
 import soot.toolkits.scalar.ForwardFlowAnalysis;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class TaintFlowAnalysis extends ForwardFlowAnalysis<Unit, Set<Taint>> {
 
-    private ConfigInterface configInterface;
-    private List<Taint> sources;
+    private final SootMethod method;
+    private final ConfigInterface configInterface;
+    private final List<Taint> sources;
 
     public TaintFlowAnalysis(Body body, ConfigInterface configInterface) {
         super(new ExceptionalUnitGraph(body));
+        this.method = body.getMethod();
         this.configInterface = configInterface;
         this.sources = new ArrayList<>();
         System.out.println(body.getMethod());
@@ -29,41 +25,51 @@ public class TaintFlowAnalysis extends ForwardFlowAnalysis<Unit, Set<Taint>> {
     @Override
     protected void flowThrough(Set<Taint> in, Unit unit, Set<Taint> out) {
         out.addAll(in);
+        Stmt stmt = (Stmt) unit;
 
-        if (unit instanceof AssignStmt) {
-            AssignStmt assignStmt = (AssignStmt) unit;
+        if (stmt instanceof IdentityStmt) {
+        }
+
+        if (stmt instanceof AssignStmt) {
+            AssignStmt assignStmt = (AssignStmt) stmt;
             Value leftOp = assignStmt.getLeftOp();
-            Value rightOp = assignStmt.getRightOp();
-            for (Taint t : in) {
-                for (ValueBox valueBox : rightOp.getUseBoxes()) {
-                    if (t.getValue().equals(valueBox.getValue())) {
-                        Taint newTaint = new Taint(unit, leftOp);
-                        t.addSuccessor(newTaint);
-                        out.add(newTaint);
-                    }
-                }
-                if (t.getValue().equals(leftOp)) {
-                    out.remove(t);
-                }
-            }
 
-            if (rightOp instanceof InvokeExpr) {
-                InvokeExpr invoke = (InvokeExpr) rightOp;
+            if (stmt.containsInvokeExpr()) {
+                InvokeExpr invoke = stmt.getInvokeExpr();
                 if (configInterface.isGetter(invoke)) {
-                    Taint newTaint = new Taint(unit, leftOp);
+                    Taint newTaint = new Taint(leftOp, stmt, method);
                     sources.add(newTaint);
                     out.add(newTaint);
+                } else {
+
+                }
+            } else {
+                for (Taint t : in) {
+                    // GEN
+                    for (ValueBox box : stmt.getUseBoxes()) {
+                        Value value = box.getValue();
+                        if (t.taints(value)) {
+                            Taint newTaint = new Taint(leftOp, stmt, method);
+                            t.addSuccessor(newTaint);
+                            out.add(newTaint);
+                        }
+                    }
+
+                    // KILL
+                    if (t.taints(leftOp)) {
+                        out.remove(t);
+                    }
                 }
             }
         }
 
-        if (unit instanceof InvokeStmt) {
+        if (stmt instanceof InvokeStmt) {
         }
 
-        if (unit instanceof ReturnStmt) {
+        if (stmt instanceof ReturnStmt) {
         }
 
-        if (unit instanceof ReturnVoidStmt) {
+        if (stmt instanceof ReturnVoidStmt) {
         }
     }
 
@@ -89,7 +95,7 @@ public class TaintFlowAnalysis extends ForwardFlowAnalysis<Unit, Set<Taint>> {
     @Override
     protected void copy(Set<Taint> source, Set<Taint> dest) {
         for (Taint t : source) {
-            dest.add(new Taint(t.getUnit(), t.getValue(), t.getSuccessors()));
+            dest.add(new Taint(t.getValue(), t.getStmt(), t.getMethod(), t.getSuccessors()));
         }
     }
 
