03f179fbac1b440f05d409965cfe2b401fe51239
zjzijielu <zj.zijielu@outlook.com>
2020-11-12 00:18:07 -0800

    case study for visiting order non determinism

--- src/main/java/taintAnalysis/PathVisitor.java
+++ src/main/java/taintAnalysis/PathVisitor.java
@@ -17,37 +17,48 @@ public class PathVisitor {
 
     public void visit(Taint t) {
         Map<Taint, Color> status = new HashMap<>();
-        dfs(t, 0, status, new Stack<>());
+        dfs(t, 0, status, new Stack<>(), true);
+        long cnt1 = cnt;
         System.out.println(t);
-        System.out.println(cnt);
+        System.out.println("run1: " + Long.toString(cnt1));
+        cnt = 0;
+
+        dfs(t, 0, status, new Stack<>(), false);
+        long cnt2 = cnt;
+//        System.out.println(t);
+        System.out.println("run2: " + Long.toString(cnt2));
+        if (cnt1 != cnt2)
+            System.out.println("##### non-deterministic");
+
     }
 
-    private void dfs(Taint t, int depth, Map<Taint, Color> status, Stack<Stmt> callerStack) {
-//        for (int i = 0; i < depth; i++) {
-//            System.out.print("-");
-//        }
-//        System.out.println(t);
+    private void dfs(Taint t, int depth, Map<Taint, Color> status, Stack<Stmt> callerStack, boolean sorted) {
+        for (int i = 0; i < depth; i++) {
+            System.out.print("-");
+        }
+        System.out.println(t);
 
         status.put(t, Color.GREY);
         Stmt curStmt = t.getStmt();
         boolean isEndPoint = true;
         ArrayList<Taint> lst = new ArrayList<>(t.getSuccessors());
-        lst.sort(Comparator.comparing(Taint::toString));
+        if (sorted)
+            lst.sort(Comparator.comparing(Taint::toString));
         for (Taint successor : lst) {
             if (status.get(successor) != Color.GREY) {
                 if (t.getTransferType() == Taint.TransferType.Call) {
                     callerStack.push(t.getStmt());
                     isEndPoint = false;
-                    dfs(successor, depth + 1, status, callerStack);
+                    dfs(successor, depth + 1, status, callerStack, sorted);
                 } else if (curStmt instanceof PhantomRetStmt && !callerStack.isEmpty()) {
                     if (callerStack.peek() == successor.getStmt()) {
                         callerStack.pop();
                         isEndPoint = false;
-                        dfs(successor, depth + 1, status, callerStack);
+                        dfs(successor, depth + 1, status, callerStack, sorted);
                     }
                 } else {
                     isEndPoint = false;
-                    dfs(successor, depth + 1, status, callerStack);
+                    dfs(successor, depth + 1, status, callerStack, sorted);
                 }
             }
         }
