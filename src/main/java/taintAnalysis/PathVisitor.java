df8b8d11dcdd3a957ca1b69432ee0f371d7aa20b
X-czh <m1a2@outlook.com>
2020-12-08 22:51:19 -0500

    visitor that visits loop only once

--- src/main/java/taintAnalysis/PathVisitor.java
+++ src/main/java/taintAnalysis/PathVisitor.java
@@ -1,5 +1,6 @@
 package taintAnalysis;
 
+import soot.SootMethod;
 import soot.jimple.Stmt;
 import taintAnalysis.utility.PhantomRetStmt;
 
@@ -7,68 +8,105 @@ import java.util.*;
 
 public class PathVisitor {
 
-    private enum Color {
-        WHITE,
-        GREY,
-        BLACK
+    private final long threshold;
+    private long cnt;
+
+    public PathVisitor() {
+        this(20000);
     }
 
-    long cnt = 0;
+    public PathVisitor(long threshold) {
+        this.threshold = threshold;
+    }
 
     public void visit(Taint t) {
-        Map<Taint, Color> status = new HashMap<>();
-        dfs(t, 0, status, new Stack<>(), true);
-        long cnt1 = cnt;
-        System.out.println(t);
-        System.out.println("run1: " + Long.toString(cnt1));
         cnt = 0;
-
-//        dfs(t, 0, status, new Stack<>(), false);
-//        long cnt2 = cnt;
-////        System.out.println(t);
-//        System.out.println("run2: " + Long.toString(cnt2));
-//        if (cnt1 != cnt2)
-//            System.out.println("##### non-deterministic");
-
+        Set<Taint> sinks = new HashSet<>();
+        Stack<Stmt> callerStack = new Stack<>();
+        Set<SootMethod> methodSet = new HashSet<>();
+        methodSet.add(t.getMethod());
+        Stack<Set<Taint>> visitedStack = new Stack<>();
+        visitedStack.push(new HashSet<>());
+        System.out.println("source: " + t);
+        dfs(t, 0, callerStack, methodSet, visitedStack, sinks);
+        System.out.println("Number of paths: " + cnt);
+        System.out.println();
     }
 
-    private void dfs(Taint t, int depth, Map<Taint, Color> status, Stack<Stmt> callerStack, boolean sorted) {
+    private void dfs(Taint t, int depth, Stack<Stmt> callerStack, Set<SootMethod> methodSet,
+                     Stack<Set<Taint>> visitedStack, Set<Taint> sinks) {
+        if (cnt > threshold) {
+            return;
+        }
+        Set<Taint> visited = visitedStack.peek();
+        if (visited.contains(t)) {
+            return;
+        }
+        visited.add(t);
+
         for (int i = 0; i < depth; i++) {
             System.out.print("-");
         }
         System.out.println(t);
 
-        status.put(t, Color.GREY);
-        Stmt curStmt = t.getStmt();
+        if (t.isSink()) {
+            sinks.add(t);
+        }
+
         boolean isEndPoint = true;
-        ArrayList<Taint> lst = new ArrayList<>(t.getSuccessors());
-        if (sorted)
-            lst.sort(Comparator.comparing(Taint::toString));
-        for (Taint successor : lst) {
-            if (status.get(successor) != Color.GREY) {
-                if (t.getTransferType() == Taint.TransferType.Call) {
-                    callerStack.push(t.getStmt());
+        Stmt currStmt = t.getStmt();
+        ArrayList<Taint> successors = new ArrayList<>(t.getSuccessors());
+        successors.sort(Comparator.comparing(Taint::toString));
+        for (Taint successor : successors) {
+            if (t.getTransferType() == Taint.TransferType.Call) {
+                // Visit callee
+                SootMethod callee = successor.getMethod();
+                if (!methodSet.contains(callee)) {
+                    callerStack.push(currStmt);
+                    methodSet.add(callee);
+                    visitedStack.push(new HashSet<>());
                     isEndPoint = false;
-                    dfs(successor, depth + 1, status, callerStack, sorted);
+                    dfs(successor, depth+1, callerStack, methodSet, visitedStack, sinks);
+                    visitedStack.pop();
+                    methodSet.remove(callee);
                     callerStack.pop();
-                } else if (curStmt instanceof PhantomRetStmt && !callerStack.isEmpty()) {
-                    if (callerStack.peek() == successor.getStmt()) {
-                        Stmt callSite = callerStack.peek();
+                }
+            } else if (currStmt instanceof PhantomRetStmt) {
+                SootMethod callee = t.getMethod();
+                SootMethod caller = successor.getMethod();
+                if (!callerStack.isEmpty()) {
+                    // Return to the previous callee
+                    Stmt callSite = callerStack.peek();
+                    if (callSite == successor.getStmt()) {
                         callerStack.pop();
+                        methodSet.remove(callee);
+                        visitedStack.pop();
                         isEndPoint = false;
-                        dfs(successor, depth + 1, status, callerStack, sorted);
+                        dfs(successor, depth+1, callerStack, methodSet, visitedStack, sinks);
+                        visitedStack.push(visited);
+                        methodSet.add(callee);
                         callerStack.push(callSite);
                     }
                 } else {
-                    isEndPoint = false;
-                    dfs(successor, depth + 1, status, callerStack, sorted);
+                    // Return to an unexplored caller
+                    if (!methodSet.contains(caller)) {
+                        methodSet.add(caller);
+                        visitedStack.push(new HashSet<>());
+                        isEndPoint = false;
+                        dfs(successor, depth+1, callerStack, methodSet, visitedStack, sinks);
+                        visitedStack.pop();
+                        methodSet.remove(caller);
+                    }
                 }
+            } else {
+                // Visit within the same method
+                isEndPoint = false;
+                dfs(successor, depth+1, callerStack, methodSet, visitedStack, sinks);
             }
         }
         if (isEndPoint) {
             cnt++;
         }
-        status.put(t, Color.BLACK);
     }
 
 }
